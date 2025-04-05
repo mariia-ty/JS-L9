3640eaf307f2af559a3d924f295986430cfcb219
mariia-ty <tymoshenkom@icloud.com>
2025-04-05 14:29:25 -0400

    added lesson

--- index.js
+++ index.js
@@ -4,12 +4,10 @@
 // const name = localStorage.getItem("name");
 // console.log(name);
 
-
 // localStorage.removeItem("name");
 
 // localStorage.clear();
 // sessionStorage.clear();
-
 
 // const user = { name: "Anna", age: 38 };
 // localStorage.setItem( "user", JSON.stringify(user) )
@@ -17,7 +15,6 @@
 // const data = localStorage.getItem("user");
 // const parsedUser = JSON.parse(data);
 // console.log(parsedUser.name);
-
 
 // const todos = [
 //     { id: 1, text: "first" },
@@ -34,8 +31,31 @@
 
 // parsedTodos.forEach(todo => console.log(todo.text));
 
+// document.getElementById("savebtn").addEventListener("click", () => {
+//   const name = document.getElementById("username").value;
+//   localStorage.setItem("username", name);
+// });
 
-document.getElementById('savebtn').addEventListener("click", () => {
-    const name = document.getElementById('username').value;
-    localStorage.set
-})
+// document.getElementById("loadbtn").addEventListener("click", () => {
+//   const name = localStorage.getItem("username");
+//   document.getElementById("output").textContent = `Your username is ${name}`;
+// });
+
+// document.getElementById("clearbtn").addEventListener("click", () => {
+//   localStorage.removeItem("username");
+//   document.getElementById("output").textContent = `No name`;
+// });
+
+const body = document.body;
+const button = document.getElementById('toggle');
+
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    body.classList.add( savedTheme );
+}
+
+button.addEventListener("click", () => {
+    body.classList.toggle('dark-theme');
+    const theme = body.classList.contains('dark-theme') ? 'dark-theme' : "";
+    localStorage.setItem('theme', theme);
+});
