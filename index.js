b0bf73a27b34e014a561d97bb56f0ad1d0894788
mariia-ty <tymoshenkom@icloud.com>
2025-04-06 13:33:27 -0400

    added practice

--- index.js
+++ index.js
@@ -46,16 +46,107 @@
 //   document.getElementById("output").textContent = `No name`;
 // });
 
-const body = document.body;
-const button = document.getElementById('toggle');
+// const body = document.body;
+// const button = document.getElementById('toggle');
 
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-    body.classList.add( savedTheme );
-}
+// const savedTheme = localStorage.getItem("theme");
+// if (savedTheme) {
+//     body.classList.add( savedTheme );
+// }
 
-button.addEventListener("click", () => {
-    body.classList.toggle('dark-theme');
-    const theme = body.classList.contains('dark-theme') ? 'dark-theme' : "";
-    localStorage.setItem('theme', theme);
+// button.addEventListener("click", () => {
+//     body.classList.toggle('dark-theme');
+//     const theme = body.classList.contains('dark-theme') ? 'dark-theme' : "";
+//     localStorage.setItem('theme', theme);
+// });
+
+
+
+//practical
+/*
+localStorage.setItem("user", "Ivan");
+
+console.log(localStorage.getItem("user"));
+
+localStorage.removeItem("user");
+localStorage.clear();
+*/
+/*
+const num = [1, 2, 3, 4];
+localStorage.setItem("numbers", JSON.stringify(num)); 
+
+const parcedNum = JSON.parse(localStorage.getItem("numbers"));
+console.log(parcedNum.reduce((sum, num) => sum + num, 0));
+*/
+/*
+const user = {
+    name: "anna", 
+    age: 25
+}; 
+sessionStorage.setItem("user", JSON.stringify(user)); 
+const parcedUser = JSON.parse(sessionStorage.getItem("user"));
+console.log(parcedUser.name);
+*/
+/*
+const save = document.getElementById("saveBtn");
+
+save.addEventListener("click", () => {
+    const user = document.getElementById("username").value;
+
+    localStorage.setItem("username", user); 
+    alert("Username Saved");
+
 });
+
+const user = localStorage.getItem("username"); 
+if (user) {
+    const title = document.getElementById("title");
+    title.textContent = user; 
+}
+    */
+/*
+const change = document.getElementById("change"); 
+const body = document.body; 
+
+const savedTheme = localStorage.getItem("theme") || "light";
+body.className = savedTheme; 
+
+
+change.addEventListener("click", () => {
+    const theme = body.className;
+    const newTheme = theme === "light" ? "dark" : "light"; 
+    body.className = newTheme; 
+    localStorage.setItem("theme", newTheme);
+});
+*/
+
+// const check = document.getElementById("checkbox");
+// const message = document.getElementById("message");
+
+// window.addEventListener("DOMContentLoaded", () => {
+
+//     const rem = localStorage.getItem('rememberMe'); 
+//     if (rem) {
+        
+//     }
+
+// });
+
+
+// Слухаємо зміну чекбоксу
+const checkbox = document.getElementById("rememberCheckbox");
+const message = document.getElementById("message");
+
+// Перевіряємо стан при завантаженні сторінки
+window.addEventListener("DOMContentLoaded", () => {
+  const remembered = localStorage.getItem("rememberMe");
+  
+  if (remembered === "true") {
+    checkbox.checked = true;
+    message.textContent = "Вітаємо знову!";
+  }
+});
+checkbox.addEventListener("change", () => {
+  localStorage.setItem("rememberMe", checkbox.checked);
+});
+
